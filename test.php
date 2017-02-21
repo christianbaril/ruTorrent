8e4e261c8ad3305b2812adaf9fd9e23ae8f89019
Chris B <christianbaril@gmail.com>
2017-02-21 18:50:21 -0500

    Finally, the XMLRPCRequest sets the owner !

--- test.php
+++ test.php
@@ -1,3 +1,10 @@
 <?php
-
-list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Text to send if user hits Cancel button';
+    exit;
+} else {
+    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+}

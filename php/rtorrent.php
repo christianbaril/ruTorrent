69a6cb3939a86ebc4f2c829b3133b2cc79663aec
Chris B <christianbaril@gmail.com>
2017-02-21 21:05:27 -0500

    Cleaned up some unused code and files

--- php/rtorrent.php
+++ php/rtorrent.php
@@ -75,7 +75,7 @@ class rTorrent
 
             if (isset($_SERVER['PHP_AUTH_USER'])) {
                 $cmd->addParameter(getCmd("d.set_custom") . '=owner,' . rawurlencode(trim($_SERVER['PHP_AUTH_USER'])));
-                $cmd->addParameter(getCmd("d.set_directory=") . $label);
+                $cmd->addParameter(getCmd("d.set_directory=/glftpd/home/rtorrent/") . $label);
             }
 
             $req->addCommand($cmd);
